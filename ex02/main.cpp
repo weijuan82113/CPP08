3e74d496f1231543d280124dbd601cd012cf4e70
Weijuan Chen <68696117+weijuan82113@users.noreply.github.com>
2024-08-26 07:46:01 +0000

    UPDATE ex02 test for different uderlying container

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
+#include <deque>
 #include "MutantStack.h"
 
 // Test code
 int main() {
-    MutantStack<int> mstack;
+    std::stack<int, std::deque<int> > mstack;
+    std::cout << "push: 5 \n";
     mstack.push(5);
+    std::cout << "push: 17 \n";
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
+    std::cout << "top: " << mstack.top() << std::endl;
+    std::cout << "pop \n";
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    std::cout << "size: " << mstack.size() << std::endl;
+
+    std::cout << "push: 3 \n";
     mstack.push(3);
+    std::cout << "push: 5 \n";
     mstack.push(5);
+    std::cout << "push: 737 \n";
     mstack.push(737);
+    std::cout << "push: 0 \n";
     mstack.push(0);
 
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<int, std::deque<int> >::iterator it = mstack.begin();
+    MutantStack<int, std::deque<int> >::iterator ite = mstack.end();
     ++it;
     --it;
-    int cout = 0;
+    int count = 0;
+    std::cout << "-----mutant stack(print by iterator)----- \n";
+    std::cout << "-----front-----" << std::endl;
     while (it != ite) {
         std::cout << *it << std::endl;
-        ++it;
-        cout ++;
+        it ++;
+        count ++;
     }
-    std::cout << "------test stack(mutant copy)------" << std::endl;
+    std::cout << "-----back-----" << std::endl;
+    std::cout << "-----mutant stack(print by iterator)-----\n\n" << std::endl;
     std::stack<int> s(mstack);
-    for (int i = 0; i < cout; i ++)
+    std::cout << "-----mutant_stack(copy)-----" << std::endl;
+    std::cout << "-----top-----" << std::endl;
+    for (int i = 0; i < count; i ++)
     {
-        std::cout << s.top() << std::endl;s.pop();
+        std::cout << s.top() << std::endl;
+        s.pop();
     }
-    std::cout << "------test stack(mutant copy)------" << std::endl;
+    std::cout << "-----bottom-----" << std::endl;
+    std::cout << "------mutant_stack(copy)------" << std::endl;
     return 0;
 }
